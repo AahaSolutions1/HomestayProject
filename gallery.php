1859f4f7a0ca1220755ff9edf7e0ad88642bed1a
jegatheswari19 <135096191+jegatheswari19@users.noreply.github.com>
2023-06-08 12:13:16 +0530

    git commit all

---
+++ gallery.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html><head>
+    <title>GALLERY</title>
+    <style>
+        img{
+            height:300px;
+            width:300px;
+            margin-left:20px;
+            margin-right:20px;
+            margin-bottom:20px;
+            margin-top:20px;
+        }
+        h1{
+            text-align: center;
+            font-weight: bolder;
+        }
+        div{
+            margin-left:50px;
+        }
+        @media screen and (max-width:1000px) {
+          body{
+            margin-left:90px;
+
+          }
+        }
+    </style>
+</head>
+    
+    <body>
+        <h1>GALLERY</h1>
+        <div>
+            <img src="images\ac.png">
+            <img src="images\home.png">
+            <img src="images\home1.png">
+            <img src="images\home2.png">
+            <img src="images\home3.png">
+            <img src="images\food.png">
+            <img src="images\kitchen.png">
+            <img src="images\bicyle.png">
+            <img src="images\family.png">
+            <img src="images\airport.png">
+        </div>
+
+    </body>
+</html>
