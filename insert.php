3b671a4739ef11f2feb3931c752a5c8a2976a622
mathumitha47 <mathumitha062004@gmail.com>
2023-06-12 13:34:32 +0530

    bookform.php

---
+++ insert.php
@@ -0,0 +1,55 @@
+<html>
+
+<head>
+    <title>Insert Page page</title>
+</head>
+
+<body>
+
+        <?php
+
+        // servername => localhost
+        // username => root
+        // password => empty
+        // database name => staff
+        $conn = mysqli_connect("localhost", "root", "", "bookingform");
+
+        // Check connection
+        if($conn === false){
+            die("ERROR: Could not connect. "
+                . mysqli_connect_error());
+        }
+
+        // Taking all 5 values from the form data(input)
+        $name = $_REQUEST['name'];
+        $phnumber = $_REQUEST['phnumber'];
+        $email = $_REQUEST['email'];
+        $aadharno = $_REQUEST['aadharno'];
+        $checkin = $_REQUEST['checkin'];
+        $checkout = $_REQUEST['checkout'];
+
+        // We are going to insert the data into our sampleDB table
+        $sql = "INSERT INTO bookingdetails VALUES ('$name',
+            '$phnumber','$email','$aadharno','$checkin','$checkout')";
+
+        // Check if the query is successful
+        if(mysqli_query($conn, $sql)){
+            echo "<h3>data stored in a database successfully."
+                . " Please browse your localhost php my admin"
+                . " to view the updated data</h3>";
+
+            echo nl2br("\n$name\n $phnumber\n "
+                . "$email\n $aadharno\n $checkin \n$checkout");
+           
+        } else{
+            echo "ERROR: Hush! Sorry $sql. "
+                . mysqli_error($conn);
+        }
+
+        // Close connection
+        mysqli_close($conn);
+        ?>
+    
+</body>
+
+</html>
